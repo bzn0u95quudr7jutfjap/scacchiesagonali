89cca13f30d8cf350893c1e29531557d44b78971
bzn0u95quudr7jutfjap <114179940+bzn0u95quudr7jutfjap@users.noreply.github.com>
2026-01-04 17:55:57 +0100

    refactor generale e selezione corretta della cella

--- scacchi.php
+++ scacchi.php
@@ -36,192 +36,293 @@ if('cli-server' !== $api){
     <pre id='data'></pre>
     <script>
 
-    const xpad =  60;
-    const ypad = -60;
-
-    const latoEsagono = 20;
-    const latoEsagonoSin = latoEsagono * 1/2;
-    const latoEsagonoCos = latoEsagono * Math.sqrt(3)/2;
-
-    const ctx = c.getContext("2d");
     const latoCanvas = window.innerHeight - 24;
     c.height = latoCanvas;
     c.width  = latoCanvas;
     c.style.width  = latoCanvas + 'px';
     c.style.height = latoCanvas + 'px';
 
-    const celle = drawScacchiera(ctx);
+    const g = function init(){
+      var g = {};
+      g.debug = 1;
+      g.c    = c;
+      g.ctx  = c.getContext("2d");
+      g.xpad =  60;
+      g.ypad = -60;
+      g.latoEsagono    = 40;
+      g.latoEsagonoSin = g.latoEsagono * 1/2;
+      g.latoEsagonoCos = g.latoEsagono * Math.sqrt(3)/2;
+      g.pezzi =
+        { bianchi:
+          [ { nome: 'A', i: 5, j:10 }
+          , { nome: 'A', i: 5, j: 9 }
+          , { nome: 'A', i: 5, j: 8 }
+          , { nome: 'P', i: 1, j:10 }
+          , { nome: 'P', i: 2, j: 9 }
+          , { nome: 'P', i: 3, j: 8 }
+          , { nome: 'P', i: 4, j: 7 }
+          , { nome: 'P', i: 5, j: 6 }
+          , { nome: 'P', i: 6, j: 6 }
+          , { nome: 'P', i: 7, j: 6 }
+          , { nome: 'P', i: 8, j: 6 }
+          , { nome: 'P', i: 9, j: 6 }
+          , { nome: 'T', i: 2, j:10 }
+          , { nome: 'T', i: 8, j: 7 }
+          , { nome: 'C', i: 3, j:10 }
+          , { nome: 'C', i: 7, j: 8 }
+          , { nome: 'D', i: 4, j:10 }
+          , { nome: 'R', i: 6, j: 9 }
+          ]
+        , neri:
+          [ { nome: 'P', i: 1, j: 4 }
+          , { nome: 'P', i: 2, j: 4 }
+          , { nome: 'P', i: 3, j: 4 }
+          , { nome: 'P', i: 4, j: 4 }
+          , { nome: 'P', i: 5, j: 4 }
+          , { nome: 'P', i: 6, j: 3 }
+          , { nome: 'P', i: 7, j: 2 }
+          , { nome: 'P', i: 8, j: 1 }
+          , { nome: 'P', i: 9, j: 0 }
+          , { nome: 'A', i: 5, j: 0 }
+          , { nome: 'A', i: 5, j: 1 }
+          , { nome: 'A', i: 5, j: 2 }
+          , { nome: 'T', i: 2, j: 3 }
+          , { nome: 'T', i: 8, j: 0 }
+          , { nome: 'C', i: 3, j: 2 }
+          , { nome: 'C', i: 7, j: 0 }
+          , { nome: 'D', i: 4, j: 1 }
+          , { nome: 'R', i: 6, j: 0 }
+          ]
+        };
+      return g;
+    }();
 
-    console.log(celle);
-
-    const pezzi =
-    { bianchi:
-      [ { nome: 'A', i: 5, j:10 }
-      , { nome: 'A', i: 5, j: 9 }
-      , { nome: 'A', i: 5, j: 8 }
-      , { nome: 'P', i: 1, j:10 }
-      , { nome: 'P', i: 2, j: 9 }
-      , { nome: 'P', i: 3, j: 8 }
-      , { nome: 'P', i: 4, j: 7 }
-      , { nome: 'P', i: 5, j: 6 }
-      , { nome: 'P', i: 6, j: 6 }
-      , { nome: 'P', i: 7, j: 6 }
-      , { nome: 'P', i: 8, j: 6 }
-      , { nome: 'P', i: 9, j: 6 }
-      , { nome: 'T', i: 2, j:10 }
-      , { nome: 'T', i: 8, j: 7 }
-      , { nome: 'C', i: 3, j:10 }
-      , { nome: 'C', i: 7, j: 8 }
-      , { nome: 'D', i: 4, j:10 }
-      , { nome: 'R', i: 6, j: 9 }
-      ]
-    , neri:
-      [ { nome: 'P', i: 1, j: 4 }
-      , { nome: 'P', i: 2, j: 4 }
-      , { nome: 'P', i: 3, j: 4 }
-      , { nome: 'P', i: 4, j: 4 }
-      , { nome: 'P', i: 5, j: 4 }
-      , { nome: 'P', i: 6, j: 3 }
-      , { nome: 'P', i: 7, j: 2 }
-      , { nome: 'P', i: 8, j: 1 }
-      , { nome: 'P', i: 9, j: 0 }
-      , { nome: 'A', i: 5, j: 0 }
-      , { nome: 'A', i: 5, j: 1 }
-      , { nome: 'A', i: 5, j: 2 }
-      , { nome: 'T', i: 2, j: 3 }
-      , { nome: 'T', i: 8, j: 0 }
-      , { nome: 'C', i: 3, j: 2 }
-      , { nome: 'C', i: 7, j: 0 }
-      , { nome: 'D', i: 4, j: 1 }
-      , { nome: 'R', i: 6, j: 0 }
-      ]
-    };
+    console.log(g);
 
     function posByIdx(i,j){
-      const x = xpad + ((latoEsagono + latoEsagonoSin) * i) + 0;
-      const y = ypad + (latoEsagonoCos * 2 * j) + (latoEsagonoCos * i);
+      const x = g.xpad + ((g.latoEsagono + g.latoEsagonoSin) * i);
+      const y = g.ypad + (g.latoEsagonoCos * 2 * j) + (g.latoEsagonoCos * i);
       return [x,y];
     }
 
-    function verticiEsagono(x,y){
+    function idxByPos(x,y){
+      const lineWidth   = g.ctx.lineWidth;
+      const strokeStyle = g.ctx.strokeStyle;
+      if(1){
+        g.ctx.lineWidth   = 1;
+
+        if(1){
+        // costante
+        g.ctx.strokeStyle = '#ff0000';
+        g.ctx.beginPath();
+        g.ctx.moveTo(        0,y);
+        g.ctx.lineTo(g.c.width,y);
+        //g.ctx.stroke();
+
+        const larghezzaEsagono  = g.latoEsagonoSin + g.latoEsagono;
+        const altezzaEsagono    = 2 * g.latoEsagonoCos;
+        var x0 = x - g.xpad;
+        x0 = x0 - (x0 % larghezzaEsagono);
+        x0 = x0 / larghezzaEsagono;
+        var y0 = y - g.ypad - ((x0 % 2) * g.latoEsagonoCos);
+        const y1 = (y0 - (y0 % altezzaEsagono)) + ((x0 % 2) * g.latoEsagonoCos);
+        g.ctx.strokeStyle = '#00ff00';
+        g.ctx.beginPath();
+        g.ctx.moveTo(        0,y1 + g.ypad);
+        g.ctx.lineTo(g.c.width,y1 + g.ypad);
+        //g.ctx.stroke();
+
+        }
+
+        if(1){
+        // ascendente
+        const a = Math.sqrt(3);
+        const b = y - a*x;
+        g.ctx.strokeStyle = '#ff0000';
+        g.ctx.beginPath();
+        g.ctx.moveTo(        0,b);
+        g.ctx.lineTo(g.c.width,a*g.c.width+b);
+        //g.ctx.stroke();
+
+        g.ctx.strokeStyle = '#00ff00';
+        var b0 = (y - g.ypad) - (a * (x - g.xpad));
+        b0 = b0 - (b0 % (2 * g.latoEsagonoCos));
+        b0 -= 2 * g.latoEsagonoCos;
+        g.ctx.beginPath();
+        g.ctx.moveTo(        0,b0 + g.ypad);
+        g.ctx.lineTo(g.c.width,a * g.c.width + b0+ g.ypad);
+        //g.ctx.stroke();
+        }
+
+        if(1){
+        // discendente
+        const a = - Math.sqrt(3);
+        const b = y - a*x;
+        g.ctx.strokeStyle = '#ff0000';
+        g.ctx.beginPath();
+        g.ctx.moveTo(        0,b);
+        g.ctx.lineTo(g.c.width,a*g.c.width+b);
+        //g.ctx.stroke();
+
+        g.ctx.strokeStyle = '#00ff00';
+        var b0 = (y - g.ypad) - (a * (x - g.xpad));
+        b0 = b0 - (b0 % (2 * g.latoEsagonoCos));
+        b0 += 2 * g.latoEsagonoCos;
+        g.ctx.beginPath();
+        g.ctx.moveTo(        0,b0 + g.ypad);
+        g.ctx.lineTo(g.c.width,a * g.c.width + b0+ g.ypad);
+        //g.ctx.stroke();
+        }
+
+        g.ctx.strokeStyle = '#3366ff';
+        for(var i = 0; i < 13; i++){
+          const a = (g.latoEsagonoCos) / (g.latoEsagono + g.latoEsagonoSin);
+          const b = (i * (2 * g.latoEsagonoCos));
+          const x0 = g.xpad + 0;
+          const y0 = g.ypad + b;
+          const x1 = g.xpad + g.c.width;
+          const y1 = g.ypad + a * g.c.width + b;
+          g.ctx.beginPath();
+          g.ctx.moveTo(x0,y0 + g.latoEsagonoCos + g.latoEsagonoCos);
+          g.ctx.lineTo(x1,y1 + g.latoEsagonoCos + g.latoEsagonoCos);
+          g.ctx.stroke();
+        }
+        for(var i = 0; i < 13; i++){
+          const a = - ((g.latoEsagonoCos) / (g.latoEsagono + g.latoEsagonoSin));
+          const b = (i * (2 * g.latoEsagonoCos));
+          const x0 = g.xpad + 0;
+          const y0 = g.ypad + b;
+          const x1 = g.xpad + g.c.width;
+          const y1 = g.ypad + a * g.c.width + b;
+          //g.ctx.beginPath();
+          //g.ctx.moveTo(x0,y0 + g.latoEsagonoCos + g.latoEsagonoCos);
+          //g.ctx.lineTo(x1,y1 + g.latoEsagonoCos + g.latoEsagonoCos);
+          //g.ctx.stroke();
+        }
+        for(var i = 0; i < 13; i++){
+          g.ctx.beginPath();
+          g.ctx.moveTo(g.xpad + (i * (g.latoEsagono + g.latoEsagonoSin)),g.ypad + 0         );
+          g.ctx.lineTo(g.xpad + (i * (g.latoEsagono + g.latoEsagonoSin)),g.ypad + g.c.height);
+          g.ctx.stroke();
+        }
+        
+      }
+      if (1){
+        // DIAGONALE ASCENDENTE SU PIANO CARTESIANO
+
+        // rimozione padding
+        const x0 = x - g.xpad;
+        const y0 = y - g.ypad;
+        const y1 = y0 - g.latoEsagonoCos;
+        
+        const altezzaEsagono   = g.latoEsagonoCos * 2;
+        const larghezzaEsagono = g.latoEsagonoSin + g.latoEsagono;
+
+        const a0     = 1 / Math.sqrt(3);
+        const bAsc   = y1 - (+a0*x0);
+        const deltaY = y1 - bAsc;
+        const bDis = Math.sqrt(deltaY*deltaY + x0*x0);
+        const j0 = Math.round(bAsc / altezzaEsagono);
+        const i0 = (bDis - (bDis % altezzaEsagono)) / altezzaEsagono;
+
+
+        let v = verticiEsagono(i0,j0);
+        v = v.map(o => ({x:o.x-g.xpad,y:o.y-g.ypad}));
+        console.log(v[3]);
+        const a1 = Math.sqrt(3);
+        const bAsc0 = v[3].y - (+a1 * v[3].x);
+        const bDis0 = v[3].y - (-a1 * v[3].x);
+
+        const yAsc =  a1 * x0 + bAsc0;
+        const yDis = -a1 * x0 + bDis0;
+        const yCst = v[3].y;
+
+        console.log('Asc ',bAsc0, yAsc, ' <- ', y0);
+        console.log('Dis ',bDis0, yDis, ' <- ', y0);
+        console.log('Cst ',yCst , yCst, ' <- ', y0);
+
+        const i = i0 + (yDis < y0 && y0 < yAsc);
+        const j = j0 + (yCst < y0 && yAsc < y0);
+
+        g.ctx.lineWidth   = lineWidth;
+        g.ctx.strokeStyle = strokeStyle;
+
+        return [i,j];
+      }
+    }
+
+    function verticiEsagono(i,j){
+      const [x,y] = posByIdx(i,j);
       const vertici = 
-        [ {x : x                            , y : y                     }
-        , {x : x+latoEsagono                , y : y                     }
-        , {x : x+latoEsagono+latoEsagonoSin , y : y + latoEsagonoCos    }
-        , {x : x+latoEsagono                , y : y + latoEsagonoCos * 2}
-        , {x : x                            , y : y + latoEsagonoCos * 2}
-        , {x : x-latoEsagonoSin             , y : y + latoEsagonoCos    }
-        , {x : x                            , y : y                     }
+        [ {x : x                                , y : y                       }
+        , {x : x+g.latoEsagono                  , y : y                       }
+        , {x : x+g.latoEsagono+g.latoEsagonoSin , y : y + g.latoEsagonoCos    }
+        , {x : x+g.latoEsagono                  , y : y + g.latoEsagonoCos * 2}
+        , {x : x                                , y : y + g.latoEsagonoCos * 2}
+        , {x : x-g.latoEsagonoSin               , y : y + g.latoEsagonoCos    }
+        , {x : x                                , y : y                       }
         ];
       return vertici;
     }
 
-    function selectCellaEsagono(ctx,cella){
-      const x = cella.x;
-      const y = cella.y;
-      ctx.beginPath();
-      ctx.moveTo(x,y); 
-      ctx.lineWidth = 6;
-      ctx.strokeStyle = '#6688ccff';
-      verticiEsagono(x,y).forEach(o => ctx.lineTo(o.x,o.y));
-      ctx.closePath();
-      ctx.stroke();
+    function selectCellaEsagono(x,y){
+      const [i,j] = idxByPos(x,y);
+      const v = verticiEsagono(i,j);
+
+      g.ctx.lineWidth = 6;
+      g.ctx.strokeStyle = '#6688ccff';
+
+      g.ctx.beginPath();
+      g.ctx.moveTo(v[0].x,v[0].y); 
+      v.forEach(function (o) { g.ctx.lineTo(o.x,o.y); });
+      g.ctx.closePath();
+      g.ctx.stroke();
     }
 
-    function drawCellaEsagonoByIdx(ctx,i,j){
-      const [x,y] = posByIdx(i,j);
-      ctx.beginPath();
-      ctx.moveTo(x,y); 
-      ctx.lineWidth = 1;
-      ctx.strokeStyle = '#000000ff';
-      verticiEsagono(x,y).forEach(o => ctx.lineTo(o.x,o.y));
-      ctx.closePath();
+    function drawCellaEsagonoByIdx(i,j){
       const colori = ['#000000ff','#888888ff','#ffffffff'];
-      ctx.fillStyle = colori[(((1 + (-j + i))%3)+3)%3];
-      ctx.fill(); 
-      ctx.stroke();
-      var retval = {};
-      retval.i=i;
-      retval.j=j;
-      retval.x=x;
-      retval.y=y;
-      return retval;
+
+      g.ctx.beginPath();
+      g.ctx.lineWidth = 1;
+      g.ctx.strokeStyle = '#000000ff';
+      g.ctx.fillStyle = colori[(((1 + (-j + i))%3)+3)%3];
+
+      const v = verticiEsagono(i,j);
+      g.ctx.moveTo(v[0].x,v[0].y); 
+      v.forEach(function (o) { g.ctx.lineTo(o.x,o.y); });
+      g.ctx.closePath();
+
+      g.ctx.fill(); 
+      g.ctx.stroke();
     }
 
-    function drawScacchiera(ctx){
-      const offsetVerticale = -3;
-      var celle = [];
-      var celleIdx = 0;
-      var i2 = 0;
-      var i3 = 0;
-
-      ctx.clearRect(0,0,c.width,c.height);
-      const df = drawCellaEsagonoByIdx;
-      for(var i = 0; i <  6; i++){celle[celleIdx++] = df(ctx,5 + i, 0);}
-      for(var i = 0; i <  7; i++){celle[celleIdx++] = df(ctx,4 + i, 1);}
-      for(var i = 0; i <  8; i++){celle[celleIdx++] = df(ctx,3 + i, 2);}
-      for(var i = 0; i <  9; i++){celle[celleIdx++] = df(ctx,2 + i, 3);}
-      for(var i = 0; i < 10; i++){celle[celleIdx++] = df(ctx,1 + i, 4);}
-      for(var i = 0; i < 11; i++){celle[celleIdx++] = df(ctx,0 + i, 5);}
-      for(var i = 0; i < 10; i++){celle[celleIdx++] = df(ctx,0 + i, 6);}
-      for(var i = 0; i <  9; i++){celle[celleIdx++] = df(ctx,0 + i, 7);}
-      for(var i = 0; i <  8; i++){celle[celleIdx++] = df(ctx,0 + i, 8);}
-      for(var i = 0; i <  7; i++){celle[celleIdx++] = df(ctx,0 + i, 9);}
-      for(var i = 0; i <  6; i++){celle[celleIdx++] = df(ctx,0 + i,10);}
-
-      return celle;
-    }
-
-    function isCellaCoprenteInPos(cella,x,y) {
-      const TODO = 0;
-      if(TODO){
-      const rad3 = Math.sqrt(3);
-
-      const y1 = cella.y;
-      const y2 = cella.y + 2 * latoEsagonoCos;
-      const centroY = y1 <= y && y <= y2;
-
-      const xEsagonoSinistra = cella.x + (latoEsagono * 1/2);
-      const yEsagonoSinistra = cella.y + (latoEsagono * (-(rad3 / 2)));
-
-      const xEsagonoDestra = cella.x + (latoEsagono * (1/2 - 1));
-      const yEsagonoDestra = cella.y + (latoEsagono * (rad3 / 2));
-
-      const ydb = (-rad3 * x) - (-rad3 * xdbaa - ydbaa);
-      const yda = (-rad3 * x) - (-rad3 * xdaab - ydaab);
-      const centroYd = ydb <= y && y <= yda;
-
-      const yab = (rad3 * x) + (rad3 * xdaab - ydbdaabaa);
-      const yaa = (rad3 * x) + (rad3 * xdbaa - ydbdaabaa);
-      const centroYa = yab <= y && y <= yaa;
-
-      console.log('y  :',y1,'<=',y,'<=',y2);
-      console.log('yd :',ydb,'<=',y,'<=',yda);
-      console.log('ya :',yab,'<=',y,'<=',yaa);
-
-      return (centroY && centroYd && centroYa);
-      }else{
-        return cella.x < x && x < (cella.x + latoEsagono) && cella.y < y && y < (cella.y + 2 * latoEsagonoCos);
-      }
+    function drawScacchiera(){
+      g.ctx.clearRect(0,0,c.width,c.height);
+      for(var i = 0; i <  6; i++){ drawCellaEsagonoByIdx(5 + i, 0); }
+      for(var i = 0; i <  7; i++){ drawCellaEsagonoByIdx(4 + i, 1); }
+      for(var i = 0; i <  8; i++){ drawCellaEsagonoByIdx(3 + i, 2); }
+      for(var i = 0; i <  9; i++){ drawCellaEsagonoByIdx(2 + i, 3); }
+      for(var i = 0; i < 10; i++){ drawCellaEsagonoByIdx(1 + i, 4); }
+      for(var i = 0; i < 11; i++){ drawCellaEsagonoByIdx(0 + i, 5); }
+      for(var i = 0; i < 10; i++){ drawCellaEsagonoByIdx(0 + i, 6); }
+      for(var i = 0; i <  9; i++){ drawCellaEsagonoByIdx(0 + i, 7); }
+      for(var i = 0; i <  8; i++){ drawCellaEsagonoByIdx(0 + i, 8); }
+      for(var i = 0; i <  7; i++){ drawCellaEsagonoByIdx(0 + i, 9); }
+      for(var i = 0; i <  6; i++){ drawCellaEsagonoByIdx(0 + i,10); }
     }
 
 c.addEventListener('mousedown', function(e) {
     const rect = this.getBoundingClientRect();
     const x = event.clientX - rect.left;
     const y = event.clientY - rect.top;
-    var cella = celle.filter(o => isCellaCoprenteInPos(o,x,y));
-    if (1 == cella.length) {
-      cella = cella[0];
-      console.log('cella',cella);
-      drawScacchiera(ctx);
-      drawPezzi(ctx,celle,pezzi);
-      selectCellaEsagono(ctx,cella);
-    }
+    drawScacchiera();
+    //drawPezzi();
+    selectCellaEsagono(x,y);
 })
 
 
-    function drawPezzi(ctx,pezzi){
-      const dimensioneTesto = (latoEsagonoCos * 2);
+    function drawPezzi(pezzi){
+      const dimensioneTesto = (g.latoEsagonoCos * 2);
       const allineamentoX = 0;
       const allineamentoY = -8;
     var deathcounter = 0;
@@ -229,29 +330,29 @@ c.addEventListener('mousedown', function(e) {
         const i = pezzo.i;
         const j = pezzo.j;
         const [x,y] = posByIdx(i,j);
-        ctx.fillText  (pezzo.nome, x + allineamentoX, y + dimensioneTesto + allineamentoY);
-        ctx.strokeText(pezzo.nome, x + allineamentoX, y + dimensioneTesto + allineamentoY);
-        ctx.fill();
-        ctx.stroke();
+        g.ctx.fillText  (pezzo.nome, x + allineamentoX, y + dimensioneTesto + allineamentoY);
+        g.ctx.strokeText(pezzo.nome, x + allineamentoX, y + dimensioneTesto + allineamentoY);
+        g.ctx.fill();
+        g.ctx.stroke();
     if(0 > --deathcounter){
         console.log(pezzo,x,y);
         throw deathcounter;
     }
       }
 
-      ctx.font = 'bold ' + dimensioneTesto + 'px monospace';
+      g.ctx.font = 'bold ' + dimensioneTesto + 'px monospace';
 
-      ctx.fillStyle   = 'white';
-      ctx.strokeStyle = 'black';
+      g.ctx.fillStyle   = 'white';
+      g.ctx.strokeStyle = 'black';
       pezzi.bianchi.forEach(disegnaPezzo);
 
-      ctx.fillStyle   = 'black';
-      ctx.strokeStyle = 'white';
+      g.ctx.fillStyle   = 'black';
+      g.ctx.strokeStyle = 'white';
       pezzi.neri.forEach(disegnaPezzo);
 
     }
 
-    drawScacchiera(ctx);
-    drawPezzi(ctx,pezzi);
+    drawScacchiera();
+    drawPezzi(g.pezzi);
 
     </script>
