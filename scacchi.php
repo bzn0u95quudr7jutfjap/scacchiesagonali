fee176318b0e2fa7fd8658726f42898b4b8364f6
bzn0u95quudr7jutfjap <114179940+bzn0u95quudr7jutfjap@users.noreply.github.com>
2026-07-25 14:48:48 +0200

    ripristina codice utile

--- scacchi.php
+++ scacchi.php
@@ -314,6 +314,8 @@ echo "<a href='?method=partita&partita=$partita_id&g=B&debug=3&t=$m'>$n</a>";
 </div>
     <script>
 
+const MOSSA_LEN = 'PBAAAA.'.length;
+const MOSSA_RGX = /[PTCADR][BN][A-Z][A-Z][A-Z][A-Z]/;
 var gPollCount = 0;
 const gPosInizialiPedoni = {
   'N' : new Set([0x14,0x24,0x34,0x44,0x54,0x63,0x72,0x81,0x90])
